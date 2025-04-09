216997bbd5cb5771e2a9a341f392ff0a569d7ace
Sathuragiri <sathuragiri@greenitive.com>
2025-04-09 21:36:40 +0530

    add railway http url

--- app.py
+++ app.py
@@ -280,4 +280,5 @@ def show_route():
 if __name__ == '__main__':
     # app.run(debug=True)
     port = int(os.environ.get('PORT', 5000))  
-    app.run(host='0.0.0.0', port=port, debug=True)
+    # app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='https://advance-route-optimization.up.railway.app/', port=port, debug=True)
