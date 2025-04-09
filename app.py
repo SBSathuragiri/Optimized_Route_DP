c9f92b7896a531f1ed61c7a1ee0fee490b303201
Sathuragiri <sathuragiri@greenitive.com>
2025-04-09 20:11:50 +0530

    second changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import networkx as nx
 import folium
 import requests
 from haversine import haversine, Unit
-
+import os
 
 app = Flask(__name__)
 
@@ -275,4 +275,6 @@ def show_route():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  
+    app.run(host='0.0.0.0', port=port, debug=True)
